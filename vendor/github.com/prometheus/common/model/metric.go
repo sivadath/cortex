cd87552f314d684562f41b777a5e67fcb288b628
sivadath <31407263+sivadath@users.noreply.github.com>
2020-03-04 16:09:42 +0530

    dotted notation support added

--- vendor/github.com/prometheus/common/model/metric.go
+++ vendor/github.com/prometheus/common/model/metric.go
@@ -24,7 +24,7 @@ var (
 	// MetricNameRE is a regular expression matching valid metric
 	// names. Note that the IsValidMetricName function performs the same
 	// check but faster than a match with this regular expression.
-	MetricNameRE = regexp.MustCompile(`^[a-zA-Z_:][a-zA-Z0-9_:]*$`)
+	MetricNameRE = regexp.MustCompile(`^[a-zA-Z_.:][a-zA-Z0-9_.:]*$`)
 )
 
 // A Metric is similar to a LabelSet, but the key difference is that a Metric is
